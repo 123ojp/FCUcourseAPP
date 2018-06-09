ce58b5b1910161c9e8a152522b9156a0a587f46c
Coffee0324 <32391571+Coffee0324@users.noreply.github.com>
2018-06-09 14:02:29 +0800

    add email

--- app/src/main/java/com/example/o123ojp/appprojet/MainActivity.java
+++ app/src/main/java/com/example/o123ojp/appprojet/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.o123ojp.appprojet;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +17,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -137,9 +141,26 @@ public class MainActivity extends AppCompatActivity
         MainActivity.this.startActivity(intent);
     }
 
-    public void turnQa(View v){
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this,QaActivity.class);
-        MainActivity.this.startActivity(intent);
+    @SuppressLint("LongLogTag")
+    protected void turnQa(View v){
+        Log.i("Send email", "");
+        String[] TO = {"zs124927755@gmail.com"};
+        String[] CC = {"xyz@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        }catch(android.content.ActivityNotFoundException ex){
+            Toast.makeText(MainActivity.this,"There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/o123ojp/appprojet/QaActivity.java
+++ app/src/main/java/com/example/o123ojp/appprojet/QaActivity.java
@@ -1,9 +1,12 @@
 package com.example.o123ojp.appprojet;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class QaActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -112,5 +116,28 @@ public class QaActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @SuppressLint("LongLogTag")
+    protected void turnemail(View v){
+        Log.i("Send email", "");
+        String[] TO = {"zs124927755@gmail.com"};
+        String[] CC = {"xyz@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        }catch(android.content.ActivityNotFoundException ex){
+            Toast.makeText(QaActivity.this,"There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
