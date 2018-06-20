5f9f6c82db452ae5916cca307678ec105d86ae1b
fcu-d0512808 <32484287+fcu-d0512808@users.noreply.github.com>
2018-06-21 02:49:41 +0800

    share word change
    github 403 gan

--- app/src/main/java/com/example/o123ojp/appprojet/CheckActivity.java
+++ app/src/main/java/com/example/o123ojp/appprojet/CheckActivity.java
@@ -154,7 +154,7 @@ public class CheckActivity extends AppCompatActivity
             share_intent.setAction(Intent.ACTION_SEND);//设置分享行为
             share_intent.setType("text/plain");//设置分享内容的类型
             share_intent.putExtra(Intent.EXTRA_SUBJECT, "分享 逢甲餘額提醒app");//添加分享内容标题
-            share_intent.putExtra(Intent.EXTRA_TEXT, "我發現了一個很棒的apk\n 他可以自動查看課程餘額並提醒你\n ");//添加分享内容
+            share_intent.putExtra(Intent.EXTRA_TEXT, MainActivity.share_detale);//添加分享内容
             //创建分享的Dialog
             share_intent = Intent.createChooser(share_intent, "分享");
             startActivity(share_intent);
